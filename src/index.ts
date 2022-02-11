f87b2c664d0e2a17d753ce168b48506828f1b6a9
Pavel Volkov <sonyahon@Pavels-MacBook-Pro.local>
2022-02-11 18:19:48 +0300

    Add entry point scripts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,24 @@
 import yargs from 'yargs';
-import {hideBin} from 'yargs/helpers';
+import { hideBin } from 'yargs/helpers';
 
 
 
 yargs(hideBin(process.argv))
-    .parse();
+  .scriptName("ific")
+  .usage('$0 <cmd> [args]')
+  .command("compile", "Compile file ific file to javascript",
+	   (yrgs) => yrgs
+	     .positional("file", {type: "string", describe: "Entry file", required: true})
+	     .option("output", {type: "string", alias: "o", describe: "Output file", required: true}),
+	   async (argv) => { console.log("Compiling", argv) })
+  .command("run", "Compile file to temporary location and run it immidiately",
+	   (yrgs) => yrgs
+	     .positional("file", {type: "string", describe: "Entry file", required: true}),
+	   async (argv) => {console.log("Running", argv)})
+  .command("repl", "Start a REPL",
+	   (yrgs) => yrgs
+	     .option("namespace", {type: "string", alias: "n", describe: "Load namepsace"}),
+	   async (argv) => {console.log("Starting REPL", argv)})
+  .help()
+  .demandCommand()
+  .parse();
